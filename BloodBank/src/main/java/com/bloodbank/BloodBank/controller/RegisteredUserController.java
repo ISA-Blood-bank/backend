224a558a4a7d6adfad91c41adef489c74cb80664
tkrgovic <tasakrgovic@gmail.com>
2022-11-15 15:22:41 +0100

    Added paths to controller for RegisteredUser

--- BloodBank/src/main/java/com/bloodbank/BloodBank/controller/RegisteredUserController.java
+++ BloodBank/src/main/java/com/bloodbank/BloodBank/controller/RegisteredUserController.java
@@ -5,16 +5,36 @@ import com.bloodbank.BloodBank.service.RegisteredUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "api/registeredusers")
+@RequestMapping("api/registeredusers")
 public class RegisteredUserController {
     @Autowired
     private RegisteredUserService regUserService;
-    
+    @GetMapping("/all")
+    public ResponseEntity<List<RegistredUser>> getAllUsers(){
+        List<RegistredUser> users = regUserService.findAll();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<RegistredUser> getUserById(@PathVariable("id") Integer id){
+        RegistredUser user = regUserService.findOne(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+    @PostMapping("/add")
+    public ResponseEntity<RegistredUser> addRegisteredUser(@RequestBody RegistredUser user) {
+        RegistredUser newUser = regUserService.addRegisteredUser(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+    @PutMapping("/update")
+    public ResponseEntity<RegistredUser> updateRegisteredUser(@RequestBody RegistredUser user) {
+        RegistredUser newUser = regUserService.updateRegisteredUser(user);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+
 }
--- BloodBank/src/main/java/com/bloodbank/BloodBank/service/RegisteredUserService.java
+++ BloodBank/src/main/java/com/bloodbank/BloodBank/service/RegisteredUserService.java
@@ -1,6 +1,7 @@
 package com.bloodbank.BloodBank.service;
 
 import com.bloodbank.BloodBank.model.RegistredUser;
+import com.bloodbank.BloodBank.model.enums.Category;
 import com.bloodbank.BloodBank.repository.RegisteredUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,7 +20,13 @@ public class RegisteredUserService {
     public List<RegistredUser> findAll(){
         return regUserRep.findAll();
     }
-    public RegistredUser save(RegistredUser registredUser){
+    public RegistredUser updateRegisteredUser(RegistredUser registredUser){
+        return regUserRep.save(registredUser);
+    }
+    public RegistredUser addRegisteredUser(RegistredUser registredUser){
+        registredUser.setCategory(Category.REGULAR);
+        registredUser.setPoints((float)0.0);
+        registredUser.setPenalties(0);
         return regUserRep.save(registredUser);
     }
     public void remove(Integer id){
