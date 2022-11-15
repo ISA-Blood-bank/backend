6fa60df21732e20e1a9c514c753c70348e014a0b
tkrgovic <tasakrgovic@gmail.com>
2022-11-15 12:45:01 +0100

    Added repo, service and controller for RegisteredUser

---
+++ BloodBank/src/main/java/com/bloodbank/BloodBank/repository/RegisteredUserRepository.java
@@ -0,0 +1,2 @@
+package com.bloodbank.BloodBank.repository;public class RegisteredUserRepository {
+}
---
+++ BloodBank/src/main/java/com/bloodbank/BloodBank/service/RegisteredUserService.java
@@ -0,0 +1,2 @@
+package com.bloodbank.BloodBank.service;public class RegisteredUserService {
+}
