8b43a5ca6c04e91ee2e86fc7a392eb98ffb1993d
DarkoCokic72 <93010785+DarkoCokic72@users.noreply.github.com>
2022-11-24 14:30:42 +0100

    Added few more endpoints in controler

--- BloodBank/src/main/java/com/bloodbank/BloodBank/controller/MedicalStaffController.java
+++ BloodBank/src/main/java/com/bloodbank/BloodBank/controller/MedicalStaffController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("api/medicalStaff")
@@ -19,6 +21,24 @@ public class MedicalStaffController {
     public ResponseEntity<MedicalStaff> addMedicalStaff(@RequestBody MedicalStaff user) {
         MedicalStaff newUser = medicalStaffService.addMedicalStaff(user);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<MedicalStaff> getOneById(@PathVariable("id") Integer id){
+        MedicalStaff ms = medicalStaffService.findById(id);
+        return new ResponseEntity<>(ms, HttpStatus.OK);
+    }
+
+    @GetMapping("/all-medical-staff/{bloodCenterId}")
+    public ResponseEntity<List<MedicalStaff>> findAll(@PathVariable("bloodCenterId") Integer bloodCenterId){
+        List<MedicalStaff> meds = medicalStaffService.findAllByBloodCenterId(bloodCenterId);
+        return  new ResponseEntity<>(meds, HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<MedicalStaff> updateMedicalStaff(@RequestBody MedicalStaff ms){
+        MedicalStaff newMedicalStaff = medicalStaffService.updateMedicalStaff(ms);
+        return new ResponseEntity<>(newMedicalStaff, HttpStatus.OK);
     }
 
 }
--- BloodBank/src/main/resources/data.sql
+++ BloodBank/src/main/resources/data.sql
@@ -3,9 +3,10 @@ insert into address (city, country, number, street ) values ('Beograd', 'Srbija'
 insert into registred_user (name, surname, jmbg, gender, email, password, address_id, occupation, job_or_school_info, points, category, penalties) values ('Tamara', 'Krgovic', '123345', 1, 'tamara@gmail.com', 'qwerty', 1, 'student', 'ftn', 0, 0, 0);
 insert into registred_user (name, surname, jmbg, gender, email, password, address_id, occupation, job_or_school_info, points, category, penalties) values ('Darko', 'Cokic', '123455', 0, 'darko@gmail.com', 'qwdfgty', 2, 'student', 'ftn', 0, 0, 0);
 insert into registred_user (name, surname, jmbg, gender, email, password, address_id, occupation, job_or_school_info, points, category, penalties) values ('Branka', 'Kljajic', '544545', 0, 'branka@gmail.com', 'qwdsfgty', 2, 'student', 'ftn', 0, 0, 0);
-insert into medical_staff (name, surname, jmbg, gender, email, password, address_id, blood_center_id) values ('Marko', 'Markovic', '1243455', 0, 'marko@gmail.com', 'qwdfhihgty',1,1);
 insert into blood_center (name, address_id, description, average_score) values ('Red cross', 1, 'good', 3);
 insert into blood_center (name, address_id, description, average_score) values ('Blue cross', 2, 'nice', 4);
 insert into blood_center (name, address_id, description, average_score) values ('Novi Sad Blood Center', 1, 'really good', 4.4);
+insert into medical_staff (name, surname, jmbg, gender, email, password, address_id, blood_center_id) values ('Marko', 'Markovic', '1243455', 0, 'marko@gmail.com', 'qwdfhihgty',1,1);
+
 commit;
 
