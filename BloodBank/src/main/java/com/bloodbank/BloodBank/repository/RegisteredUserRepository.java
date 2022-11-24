33e46195da24a270c9f83947a6e0727b30f59f84
BrankaKljajic <branka.kljajic00@gmail.com>
2022-11-24 18:51:33 +0100

    Moved search for users to backend

--- BloodBank/src/main/java/com/bloodbank/BloodBank/repository/RegisteredUserRepository.java
+++ BloodBank/src/main/java/com/bloodbank/BloodBank/repository/RegisteredUserRepository.java
@@ -1,7 +1,14 @@
 package com.bloodbank.BloodBank.repository;
 
+import com.bloodbank.BloodBank.model.BloodCenter;
 import com.bloodbank.BloodBank.model.RegistredUser;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface RegisteredUserRepository extends JpaRepository<RegistredUser, Integer> {
+    @Query("select r from RegistredUser r where r.name = ?1 and r.surname = ?2 ")
+    public List<RegistredUser> search(String inputName, String inputSurname);
 }
