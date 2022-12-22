09f94a11fd6d062eb3ecbab7ce7fb2ff48a94388
DarkoCokic72 <93010785+DarkoCokic72@users.noreply.github.com>
2022-12-22 18:12:17 +0100

    bugfix

--- BloodBank/src/main/java/com/bloodbank/BloodBank/repository/AppointmentReportRepository.java
+++ BloodBank/src/main/java/com/bloodbank/BloodBank/repository/AppointmentReportRepository.java
@@ -1,9 +1,5 @@
 package com.bloodbank.BloodBank.repository;
 
-<<<<<<< HEAD
-=======
-import com.bloodbank.BloodBank.model.Appointment;
->>>>>>> 53e4afbd21cbde7b04ea248b6c659eb0cf7bf310
 import com.bloodbank.BloodBank.model.AppointmentReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
--- BloodBank/src/main/java/com/bloodbank/BloodBank/repository/AppointmentRepository.java
+++ BloodBank/src/main/java/com/bloodbank/BloodBank/repository/AppointmentRepository.java
@@ -3,14 +3,11 @@ package com.bloodbank.BloodBank.repository;
 import com.bloodbank.BloodBank.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-<<<<<<< HEAD
+
 import java.util.List;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
     List<Appointment> findAllByBloodCenterId(int bloodCenterId);
 
     List<Appointment> findAllByMedicalStaffId(int medicalStaffId);
-=======
-public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
->>>>>>> 53e4afbd21cbde7b04ea248b6c659eb0cf7bf310
 }
